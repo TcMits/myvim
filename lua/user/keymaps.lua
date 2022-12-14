23be65fbb687de6e272feafc1ec93b7a92110d81
Lâm Hoàng Phúc <64113128+TcMits@users.noreply.github.com>
2022-12-14 18:53:36 +0700

    update

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -47,10 +47,6 @@ keymap("n", "<S-q>", "<cmd>Bdelete!<CR>", opts)
 -- Better paste
 keymap("v", "p", '"_dP', opts)
 
--- Insert --
--- Press jk fast to enter
-keymap("i", "jk", "<ESC>", opts)
-
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
