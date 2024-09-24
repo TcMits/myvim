275b876d96d3956cafdb745efe15e9d8bc2ca3c9
TcMits <eddiebrock2001@gmail.com>
2024-09-24 18:11:50 +0700

    update

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -1,11 +1,3 @@
 local gitsigns = require("gitsigns")
 
-gitsigns.setup {
-  signs = {
-    add = { text = "▎" },
-    change = { text = "▎" },
-    delete = { text = "契" },
-    topdelete = { text = "契" },
-    changedelete = { text = "▎" },
-  },
-}
+gitsigns.setup()
