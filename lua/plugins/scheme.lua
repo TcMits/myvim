64a4e425392e0ec73cdbe58949e0636ef2f11941
TcMits <eddiebrock2001@gmail.com>
2026-03-31 23:49:03 +0700

    update treesitter

--- lua/plugins/scheme.lua
+++ lua/plugins/scheme.lua
@@ -1,3 +1,35 @@
+FILE_TYPES = {
+	"c",
+	"query",
+	"markdown",
+	"markdown_inline",
+	"rust",
+	"bash",
+	"lua",
+	"python",
+	"toml",
+	"yaml",
+	"json",
+	"html",
+	"css",
+	"javascript",
+	"typescript",
+	"tsx",
+	"cpp",
+	"cmake",
+	"go",
+	"java",
+	"latex",
+	"regex",
+	"make",
+	"templ",
+	"vim",
+	"vimdoc",
+	"astro",
+	"sql",
+  "svelte",
+}
+
 return {
 	{
 		"rebelot/kanagawa.nvim",
@@ -18,45 +50,22 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		lazy = false,
-    build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+		build = function()
+			require("nvim-treesitter.install").update({ with_sync = true })()
+		end,
 		branch = "main",
 		config = function()
-			require("nvim-treesitter")
-				.install({
-					"c",
-					"query",
-					"markdown",
-					"markdown_inline",
-					"rust",
-					"bash",
-					"lua",
-					"python",
-					"toml",
-					"yaml",
-					"json",
-					"html",
-					"css",
-					"javascript",
-					"typescript",
-					"tsx",
-					"cpp",
-					"cmake",
-					"go",
-					"java",
-					"latex",
-					"regex",
-					"make",
-					"templ",
-					"vim",
-					"vimdoc",
-					"astro",
-				})
-				:wait(300000)
+			require("nvim-treesitter").install(FILE_TYPES):wait(300000)
 
 			-- Enable syntax highlighting
 			vim.cmd("syntax on")
+
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = FILE_TYPES,
+				callback = function()
+					vim.treesitter.start()
+				end,
+			})
 		end,
 	},
 }
