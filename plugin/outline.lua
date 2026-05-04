4bdf295a25d510fefce1338b47997edd9df269a8
TcMits <eddiebrock2001@gmail.com>
2026-05-04 11:41:01 +0700

    update

--- plugin/outline.lua
+++ plugin/outline.lua
@@ -2,4 +2,16 @@ vim.pack.add({
 	"https://github.com/hedyhli/outline.nvim",
 })
 
+require("outline").setup({
+	outline_window = {
+		auto_width = {
+			-- Dynamically resize window width to fit content
+			enabled = true,
+			-- Maximum width (columns or percent if relative_width)
+			max_width = 30,
+			-- Include symbol details in width calculation
+			include_symbol_details = false,
+		},
+	},
+})
 vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
